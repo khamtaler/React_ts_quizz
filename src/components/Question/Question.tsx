d46e3dcd78d538d81c5ff5bf40beeed8163b4d3b
Tomasz Sroka <tsroka41@gmail.com>
2022-12-02 18:19:17 +0100

    Added question checking, setting nav styles, styles

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,16 +1,17 @@
 import { Questions } from '../../models/Questions';
 import { ReactNode } from 'react';
-
+import Quizz_form from '../Quizz/form.module.css';
+import singleQuestionStyles from './SingleQuestion.module.css';
 interface Props extends Pick<Questions, 'id' | 'question'> {
 	children: ReactNode;
 }
 
 export default function Question({ id, question, children }: Props) {
 	return (
-		<section id={id}>
-			<div>
+		<section id={id} className={` ${singleQuestionStyles.Question}   question`}>
+			<div className={`${Quizz_form.Quizz_form} ${singleQuestionStyles.Question_container} `}>
 				<h2 dangerouslySetInnerHTML={{ __html: question }}></h2>
-				<div>{children}</div>
+				<div className={singleQuestionStyles.Question_answers}>{children}</div>
 			</div>
 		</section>
 	);
--- src/components/Quizz/Quiz.tsx
+++ src/components/Quizz/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, ReactElement } from 'react';
 import { FormData } from '../../models/FormData';
 import { nanoid } from 'nanoid';
 import Question from '../Question/Question';
@@ -6,6 +6,17 @@ import AnswerButton from '../AnswerButton/AnswerButton';
 import { Questions } from '../../models/Questions';
 import { MappedAnswer } from '../../models/MappedAnswers';
 import { DataArray, Data } from '../../models/DataArray';
+import navStyles from './navigation.module.css';
+import questionsStyles from './/Questions.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faSquare,
+	faArrowLeft,
+	faArrowRight,
+	faCheck,
+	faRotateLeft,
+	faGear,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface Props extends FormData {
 	setShowQuizz: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,6 +26,11 @@ export default function Quiz({ numberOfQuestions, difficulty, setShowQuizz }: Pr
 	const [data, setData] = useState<DataArray>([]);
 	const [remake, setRemake] = useState<boolean>(false);
 	const [validation, setValidation] = useState<boolean>(false);
+	const [navigation, setNavigation] = useState<ReactElement[]>([]);
+	const [sections, setSections] = useState<Array<Element>>([]);
+	const [currentSection, setCurrentSection] = useState(0);
+	const [points, setPoints] = useState<number>(0);
+	const navigationTop = useRef<HTMLDivElement>(null);
 
 	const mapAnswers = (item: Questions) => {
 		const { correct_answer, incorrect_answers } = item;
@@ -86,6 +102,82 @@ export default function Quiz({ numberOfQuestions, difficulty, setShowQuizz }: Pr
 		);
 	});
 
+	function checkAnswers() {
+		setValidation(true);
+	}
+
+	useEffect(() => {
+		setData((questions) => {
+			return questions.map((question) => {
+				return {
+					...question,
+					isPoint: question.answers.every((answer) => {
+						if (answer.isChecked && answer.isCorrect) {
+							setPoints((points) => points + 1);
+							return true;
+						}
+						if (!answer.isChecked && !answer.isCorrect) {
+							return true;
+						}
+						return false;
+					}),
+				};
+			});
+		});
+	}, [validation]);
+
+	function checkIfPoint(question: Data): string | undefined {
+		if (question.isPoint == null) {
+			return undefined;
+		} else if (question.isPoint) {
+			return `${navStyles.Navigation___correct}`;
+		} else {
+			return `${navStyles.Navigation___inCorrect}`;
+		}
+	}
+
+	function newQuiz() {
+		setRemake((prev) => !prev);
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+		setCurrentSection(0);
+		setPoints(0);
+	}
+
+	useEffect(() => {
+		setNavigation(
+			data.map((question: Data, index) => {
+				const styles: string | undefined = checkIfPoint(question);
+				return (
+					<a
+						key={question.id}
+						href={`#${question.id}`}
+						className={styles}
+						onClick={() => setCurrentSection(index)}
+					>
+						<FontAwesomeIcon icon={faSquare} />
+					</a>
+				);
+			})
+		);
+		setSections([...document.querySelectorAll('.question')] as Array<Element>);
+	}, [data]);
+
+	useEffect(() => {
+		if (sections.length !== 0) {
+			sections[currentSection].scrollIntoView({ behavior: 'smooth', block: 'start' });
+			const { id: sectionId } = sections[currentSection];
+			const hrefArray = document.querySelectorAll(`.nav a`);
+			hrefArray.forEach((element) => {
+				element.classList.remove('active');
+			});
+			const activeSection = document.querySelector(`[href="#${sectionId}"]`) as HTMLElement | null;
+			if (activeSection != undefined) {
+				activeSection.classList.add('active');
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [currentSection]);
+
 	function chooseAnswer(questionId: string, answerId: string) {
 		setData((questions) => {
 			return questions.map((question) => {
@@ -107,5 +199,86 @@ export default function Quiz({ numberOfQuestions, difficulty, setShowQuizz }: Pr
 		});
 	}
 
-	return <div>{questions}</div>;
+	return questions.length !== 0 ? (
+		<main className={questionsStyles.Questions_main}>
+			<div ref={navigationTop} className={`${navStyles.Navigation} nav`}>
+				{navigation}
+				<a href={'#final'} onClick={() => setCurrentSection(sections.length - 1)}>
+					<FontAwesomeIcon icon={faSquare} />
+				</a>
+			</div>
+			<div className={questionsStyles.Questions_container}>
+				{questions}
+				<div id="final" className={`${questionsStyles.Questions_finalStep} question`}>
+					<div className={questionsStyles.Questions_checkContainer}>
+						{validation && (
+							<div className={questionsStyles.Questions_score}>
+								{' '}
+								{(points / data.length) * 100 >= 50 ? <h2> Hurray!</h2> : <h2>Whoopsie!</h2>}
+							</div>
+						)}
+						{validation && (
+							<h2>
+								<span>Your score is: </span>
+								<span>
+									{points}/{data.length}
+								</span>
+								<br />
+								<span>that's: </span>
+								<span style={{ color: (points / data.length) * 100 >= 50 ? '#1bf11b' : 'red' }}>
+									{Math.round((points / data.length) * 100)}%
+								</span>
+							</h2>
+						)}
+						<button
+							className={`${questionsStyles.Questions_finalStep___check} button`}
+							onClick={() => checkAnswers()}
+						>
+							<FontAwesomeIcon icon={faCheck} /> <span>check answers</span>
+						</button>
+						<button
+							className={` ${questionsStyles.Questions_finalStep___replay} button`}
+							onClick={() => {
+								setValidation(false);
+
+								newQuiz();
+							}}
+						>
+							<FontAwesomeIcon icon={faRotateLeft} /> Replay quizz
+						</button>
+
+						<button
+							className={` ${questionsStyles.Questions_finalStep___reset} button`}
+							onClick={() => setShowQuizz(false)}
+						>
+							{' '}
+							<FontAwesomeIcon icon={faGear} /> Reset settings
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className={navStyles.NavigationButtons}>
+				{currentSection !== 0 && (
+					<button
+						className={`${navStyles.NavigationButtons___top} button`}
+						onClick={() => setCurrentSection((prev) => prev - 1)}
+					>
+						<FontAwesomeIcon icon={faArrowLeft} /> previous
+					</button>
+				)}
+				{currentSection !== sections.length - 1 && (
+					<button
+						className={`${navStyles.NavigationButtons___bottom} button`}
+						onClick={() => setCurrentSection((prev) => prev + 1)}
+					>
+						next <FontAwesomeIcon icon={faArrowRight} />
+					</button>
+				)}
+			</div>
+		</main>
+	) : (
+		<main className={questionsStyles.Questions_main}>
+			<h2 className={questionsStyles.Questions_loading}>Loading...</h2>
+		</main>
+	);
 }
